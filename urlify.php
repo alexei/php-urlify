384a39600adaab0b6844c26cf3fe284360803883
Alexei <hello@alexei.ro>
2013-09-26 16:29:58 +0300

    made transliteration optional

--- urlify.php
+++ urlify.php
@@ -1,7 +1,9 @@
 <?php
 
-function urlify($str = '', $glue = '-') {
-    $str = Unidecode::decode($str);
+function urlify($str = '', $glue = '-', $transliterate = true) {
+    if ($transliterate) {
+        $str = Unidecode::decode($str);
+    }
     $str = preg_replace('/[^a-zA-Z0-9 _-]/', '', $str);
     $str = preg_replace('/\s+/', $glue, trim($str));
 
